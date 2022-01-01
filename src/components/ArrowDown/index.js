1a19cff0edb57f86944f6b0fd03f2503d4df02bc
Alexandre Julião <alexandrejuliao@MacBook-Air-de-Alexandre.local>
2021-12-31 22:32:49 -0300

    :construction: Changed Player

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -3,7 +3,7 @@ function ArrowDown(currentPlayer = 1) {
   return /*html*/ `
   <img 
     class="arrow-down" 
-    data-currentPlayer="${currentPlayer}"
+    data-currentplayer="${currentPlayer}"
     src="images/arrow.svg" 
     alt="Icone de uma seta para baixo"/>
   `
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -6,8 +6,8 @@ import './style.css'
 function ScoreBoard() {
   return /*html*/ `
     <header class="score-board">
-    ${ArrowDown(2)}
-      ${PlayerName('Player 1')}
+      ${ArrowDown(2)}
+      ${PlayerName('Player 3')}
       ${PlayerScore(2)}
       ${VsPlayer()}
       ${PlayerScore(3)}
